5595b443c3cb51c6d89f5fcd5a0650d70862ece1
ishiimizuki <ishiimizuki1192@gmail.com>
2016-10-01 20:08:13 +0900

    ファイル追加

---
+++ Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by ISHIIMIZUKI on 2016/10/01.
+//  Copyright © 2016年 tontokoton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
